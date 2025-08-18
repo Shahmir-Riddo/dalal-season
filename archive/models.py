f0d2da89ad8e1e0db2f5012af0d1fb77a20ca61f
Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2025-08-18 21:52:27 +0600

    Update models.py

--- archive/models.py
+++ archive/models.py
@@ -17,11 +17,6 @@ class District(models.Model):
 
     def __str__(self):
         return self.name
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        return super().save(*args, **kwargs)
 
 
 
