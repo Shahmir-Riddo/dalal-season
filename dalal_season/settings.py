9665a628705465be3ef1116dec4a2c07be85d9f6
Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2025-08-21 12:07:50 +0600

    Update settings.py

--- dalal_season/settings.py
+++ dalal_season/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kpy7j%_xtc%f@@+81sc8&=)uo84c+wclqo7mf*_)nyinefk85f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
